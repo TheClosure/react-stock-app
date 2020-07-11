ff9977ab857b1b590821a99c7ce3df613d868643
Dmitriy <dmitrijs.asanovs@gmail.com>
2020-07-11 22:32:34 +0300

    update styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const App = () => {
     return (
-        <div>Hello world!</div>
+        <div style={ { display: 'flex', justifyContent: 'center' } }>
+            <div style={ { width: '70%' } }>
+                <div>Hello world!</div>
+            </div>
+        </div>
     );
 };
 
